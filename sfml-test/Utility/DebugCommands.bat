b587ee40a98f7b550e5c9d32fa1a4546261c8b5d
martin <martinconcetti97@gmail.com>
2021-11-29 01:53:50 -0300

    Bat para debug y release

---
+++ sfml-test/Utility/DebugCommands.bat
@@ -0,0 +1,11 @@
+set ProjectDir=%~1
+set TargetDir=%~2
+set DLLDirSFML=%~3
+
+xcopy "%ProjectDir%res\" "%TargetDir%res\" /E /Y
+copy "%DLLDirSFML%\openal32.dll" "%TargetDir%openal32.dll" /Y
+copy "%DLLDirSFML%\sfml-audio-d-2.dll" "%TargetDir%sfml-audio-d-2.dll" /Y
+copy "%DLLDirSFML%\sfml-graphics-d-2.dll" "%TargetDir%sfml-graphics-d-2.dll" /Y
+copy "%DLLDirSFML%\sfml-network-d-2.dll" "%TargetDir%sfml-network-d-2.dll" /Y
+copy "%DLLDirSFML%\sfml-system-d-2.dll" "%TargetDir%sfml-system-d-2.dll" /Y
+copy "%DLLDirSFML%\sfml-window-d-2.dll" "%TargetDir%sfml-window-d-2.dll" /Y
---
+++ sfml-test/Utility/ReleaseCommands.bat
@@ -0,0 +1,11 @@
+set ProjectDir=%~1
+set TargetDir=%~2
+set DLLDirSFML=%~3
+
+xcopy "%ProjectDir%%res\" "%TargetDir%%res\" /E /Y
+copy "%DLLDirSFML%\openal32.dll" "%TargetDir%openal32.dll" /Y
+copy "%DLLDirSFML%\sfml-audio-2.dll" "%TargetDir%sfml-audio-2.dll" /Y
+copy "%DLLDirSFML%\sfml-graphics-2.dll" "%TargetDir%sfml-graphics-2.dll" /Y
+copy "%DLLDirSFML%\sfml-network-2.dll" "%TargetDir%sfml-network-2.dll" /Y
+copy "%DLLDirSFML%\sfml-system-2.dll" "%TargetDir%sfml-system-2.dll" /Y
+copy "%DLLDirSFML%\sfml-window-2.dll" "%TargetDir%sfml-window-2.dll" /Y
